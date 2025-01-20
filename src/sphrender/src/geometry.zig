44e442329b3121a39a14d11f78deda8bc4f476f4
Mick Sayson <mick@sayson.com>
2025-01-20 13:31:13 -0800

    Add cone segment sampler
    While not used in sphimp, sphimp librarys happen to be exported to other
    projects where this is useful

--- src/sphrender/src/geometry.zig
+++ src/sphrender/src/geometry.zig
@@ -105,3 +105,145 @@ pub const TentGenerator = struct {
         return .{ ret2[0], ret2[1], depth };
     }
 };
+
+pub const ConeSegmentGenerator = struct {
+    // Sample circle of radius 1.0, we will multiply elements as needed
+    normalized_sampler: CircleSampler,
+    // First output of normalized_sampler since we need to close the loop
+    initial_sample: sphmath.Vec2,
+
+    inner_radius: f32,
+    outer_radius: f32,
+
+    inner_depth: f32,
+    outer_depth: f32,
+
+    prev_sample: sphmath.Vec2,
+    curr_sample: sphmath.Vec2,
+    state: State = .new_sample,
+
+    const State = enum {
+        new_sample,
+        output_second_tri,
+        output_final,
+        finished,
+    };
+
+    pub fn init(num_samples: usize, inner_radius: f32, outer_radius: f32, inner_depth: f32, outer_depth: f32) ConeSegmentGenerator {
+        var sampler = CircleSampler{
+            .radius = 1.0,
+            .num_samples = num_samples,
+        };
+        const initial_sample = sampler.next().?;
+
+        return .{
+            .normalized_sampler = sampler,
+            .initial_sample = initial_sample,
+            .curr_sample = initial_sample,
+            .prev_sample = undefined,
+            .inner_radius = inner_radius,
+            .outer_radius = outer_radius,
+            .inner_depth = inner_depth,
+            .outer_depth = outer_depth,
+        };
+    }
+
+    pub const Triangle = [3]sphmath.Vec3;
+
+    pub fn next(self: *ConeSegmentGenerator) ?Triangle {
+        const sample = retrieveNextSample(self.state, &self.normalized_sampler, self.initial_sample);
+        const this_state = self.state;
+
+        if (sample.new_sample) |v| {
+            self.prev_sample = self.curr_sample;
+            self.curr_sample = v;
+        }
+
+        self.state = sample.new_state;
+
+        return generateTriangle(
+            this_state,
+            self.prev_sample,
+            self.curr_sample,
+            self.inner_radius,
+            self.outer_radius,
+            self.inner_depth,
+            self.outer_depth,
+        );
+    }
+
+    const Sample = struct {
+        new_sample: ?sphmath.Vec2,
+        new_state: State,
+    };
+
+    fn retrieveNextSample(current_state: State, sampler: *CircleSampler, initial_sample: sphmath.Vec2) Sample {
+        var new_state = current_state;
+        var new_sample: ?sphmath.Vec2 = null;
+
+        switch (current_state) {
+            .new_sample => {
+                if (sampler.next()) |v| {
+                    new_state = .output_second_tri;
+                    new_sample = v;
+                } else {
+                    new_state = .output_final;
+                    new_sample = initial_sample;
+                }
+            },
+            .output_second_tri => {
+                new_state = .new_sample;
+            },
+            .output_final => {
+                new_state = .finished;
+            },
+            .finished => {},
+        }
+
+        return .{
+            .new_sample = new_sample,
+            .new_state = new_state,
+        };
+    }
+
+    fn generateTriangle(
+        state: State,
+        prev_sample: sphmath.Vec2,
+        curr_sample: sphmath.Vec2,
+        inner_radius: f32,
+        outer_radius: f32,
+        inner_depth: f32,
+        outer_depth: f32,
+    ) ?Triangle {
+        switch (state) {
+            .new_sample => {
+                // Sampler moves CCW, triangles should have CW winding order
+                const a = prev_sample * sphmath.Vec2{ outer_radius, outer_radius };
+                const b = prev_sample * sphmath.Vec2{ inner_radius, inner_radius };
+                const c = curr_sample * sphmath.Vec2{ inner_radius, inner_radius };
+
+                return .{
+                    addDepth(a, outer_depth),
+                    addDepth(b, inner_depth),
+                    addDepth(c, inner_depth),
+                };
+            },
+            .output_second_tri, .output_final => {
+                // Sampler moves CCW, triangles should have CW winding order
+                const a = prev_sample * sphmath.Vec2{ outer_radius, outer_radius };
+                const b = curr_sample * sphmath.Vec2{ inner_radius, inner_radius };
+                const c = curr_sample * sphmath.Vec2{ outer_radius, outer_radius };
+
+                return .{
+                    addDepth(a, outer_depth),
+                    addDepth(b, inner_depth),
+                    addDepth(c, outer_depth),
+                };
+            },
+            .finished => return null,
+        }
+    }
+    fn addDepth(item: sphmath.Vec2, depth: f32) sphmath.Vec3 {
+        return .{ item[0], item[1], depth };
+    }
+};
