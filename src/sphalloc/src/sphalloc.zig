d57a17cdff1576f06fcf906195818833d1eb1d90
Mick Sayson <mick@sayson.com>
2025-03-13 00:38:50 -0700

    Add some ScratchAlloc helpers
    Sometimes it's useful to make a big buffer, and squish it back down into
    something smaller once you know the size

--- src/sphalloc/src/sphalloc.zig
+++ src/sphalloc/src/sphalloc.zig
@@ -188,6 +188,25 @@ pub const ScratchAlloc = struct {
         self.backing.reset();
     }
 
+    pub fn allocMax(self: *ScratchAlloc, comptime T: type) []T {
+        return self.allocGreedy(T, std.math.maxInt(usize));
+    }
+
+    pub fn allocGreedy(self: *ScratchAlloc, comptime T: type, max: usize) []T {
+        const start_addr: usize = @intFromPtr(self.backing.buffer.ptr);
+        const current_head = start_addr + self.backing.end_index;
+        const alloc_start = std.mem.alignForward(usize, current_head, @alignOf(T));
+        const capacity = (self.backing.buffer.len -| (alloc_start - start_addr)) / @sizeOf(T);
+        const size = @min(capacity, max);
+        return self.backing.allocator().alloc(T, size) catch unreachable;
+    }
+
+    pub fn shrinkTo(self: *ScratchAlloc, ptr: ?*anyopaque) void {
+        const base: usize = @intFromPtr(self.backing.buffer.ptr);
+        const ptr_u: usize = @intFromPtr(ptr);
+        self.backing.end_index = ptr_u - base;
+    }
+
     pub const Checkpoint = usize;
 
     pub fn checkpoint(self: *ScratchAlloc) Checkpoint {
