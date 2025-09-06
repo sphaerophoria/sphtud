c41cf9546ff85fe5b08895fca16c437632d14d65
Mick Sayson <mick@sayson.com>
2025-09-05 22:02:47 -0700

    Allow pointer retrievers in guitext

--- src/gui/src/gui_text.zig
+++ src/gui/src/gui_text.zig
@@ -173,6 +173,10 @@ fn getText(text_retriever: anytype) []const u8 {
             if (child_info == .pointer and child_info.pointer.child == u8 and child_info.pointer.size == .slice) {
                 return text_retriever.*.*;
             }
+
+            if (child_info == .@"struct") {
+                return getText(text_retriever.*);
+            }
         },
         else => {},
     }
